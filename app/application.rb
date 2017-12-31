baa36102480d827eb0fafe092627791e2ee093b6
Richard Burd <richardburdx@yahoo.com>
2017-12-31 21:34:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 require_relative '../config/environment'
 
 class Application
-  @@itrems = [Item.new("Baklawah", "$32:34"), Item.new("Sushi", "$15.99"), Item.new("Knegelachk", "$12.99")]
+  @@items = [Item.new("Baklawah", "$32:34"), Item.new("Sushi", "$15.99"), Item.new("Knegelachk", "$12.99")]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
